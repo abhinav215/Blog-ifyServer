3451c6d91579a275df65d56b3fcca5270819c692
Abhinav <abhinav0606june@gmail.com>
2022-07-01 01:59:28 +0530

    make it better

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type,Accept"
+  );
+  next();
+});
+
 app.use(
   cors()
   //   {
